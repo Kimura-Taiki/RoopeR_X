f2cac8d848c76906b4af66ad68c2af20c62e6ef2
“Kimura-Taiki” <taiguimucun850@gmail.com>
2022-01-07 07:49:59 +0000

    座の追加機能を実装

--- app/controllers/zas_controller.rb
+++ app/controllers/zas_controller.rb
@@ -1,11 +1,19 @@
 class ZasController < ApplicationController
   def index
+    @zas = Za.all
+    @za = Za.new
   end
 
   def new
   end
 
   def create
+    @za = Za.new(za_params)
+    if @za.save
+      redirect_to zas_path
+    else
+      render :index
+    end
   end
 
   def show
@@ -22,5 +30,6 @@ class ZasController < ApplicationController
 
   private
   def za_params
+    params.require(:za).permit(:name, :paranoia_limit, :image)
   end
 end
--- app/models/za.rb
+++ app/models/za.rb
@@ -3,4 +3,6 @@ class Za < ApplicationRecord
   # has_many :za_category_attributions
   # has_many :categories, through: :za_category_attributions
   # has_many :chara
+
+  attachment :image
 end
