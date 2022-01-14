36dd8397bb346eade113e9bb43d08f07677c0104
“Kimura-Taiki” <taiguimucun850@gmail.com>
2022-01-14 11:59:41 +0000

    ymlファイルあたりまで作ってenumの日本語化は完成っぽい

--- app/models/position.rb
+++ app/models/position.rb
@@ -3,4 +3,8 @@ class Position < ApplicationRecord
   # has_many :packages, through: :package_position_owns
   # has_many :rule_position_owns
   # has_many :rules, through: :rule_position_owns
+  
+  enum refusal: { neg: 0, optional: 1, mandatory: 2 }, _prefix: true
+  enum unkillable: { neg: 0, undead: 1 }, _prefix: true
+  enum obstinate: { neg: 0, sinner: 1 }, _prefix: true
 end
