ade8921effd314412b1f48da034ac8512541c176
“Kimura-Taiki” <taiguimucun850@gmail.com>
2022-01-15 17:32:22 +0000

    CSVからのseed導入、refile画像データの種付け成功

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,26 @@
 
 # Category
 # Category.create(:name => "(null)")
-Category.create(:name => "学生")
-Category.create(:name => "少年")
-Category.create(:name => "少女")
-Category.create(:name => "大人")
-Category.create(:name => "男性")
-Category.create(:name => "女性")
-Category.create(:name => "虚構")
-Category.create(:name => "造物")
-Category.create(:name => "動物")
-Category.create(:name => "樹木")
-Category.create(:name => "妹")
+
+# Category.create(:name => "学生")
+# Category.create(:name => "少年")
+# Category.create(:name => "少女")
+# Category.create(:name => "大人")
+# Category.create(:name => "男性")
+# Category.create(:name => "女性")
+# Category.create(:name => "虚構")
+# Category.create(:name => "造物")
+# Category.create(:name => "動物")
+# Category.create(:name => "樹木")
+# Category.create(:name => "妹")
+
+require "csv"
+
+CSV.foreach('db/seeds/座の一覧(新).csv', headers: true) do |row|
+  Za.create(
+    name: row['名称'],
+    paranoia_limit: row['不安臨界'],
+    # image_id: row['画像名'],
+    image: File.open("./app/assets/images/#{row['画像名']}")
+  )
+end
