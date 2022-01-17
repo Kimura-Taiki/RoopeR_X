a24bf97a3f263cf9a124d7bae1935be704b3dc7b
“Kimura-Taiki” <taiguimucun850@gmail.com>
2022-01-17 03:48:34 +0000

    役職の表をnumbersで作って上げて、seedからindexのviewまで持っていけた

--- app/models/position.rb
+++ app/models/position.rb
@@ -4,7 +4,7 @@ class Position < ApplicationRecord
   # has_many :rule_position_owns
   # has_many :rules, through: :rule_position_owns
   
-  enum refusal: { neg: 0, optional: 1, mandatory: 2 }, _prefix: true
+  enum refusal: { neg: 0, optional: 1, mandatory: 2, puppet: 3 }, _prefix: true
   enum unkillable: { neg: 0, undead: 1 }, _prefix: true
   enum obstinate: { neg: 0, sinner: 1 }, _prefix: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,41 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Category
-# Category.create(:name => "(null)")
-
-# Category.create(:name => "学生")
-# Category.create(:name => "少年")
-# Category.create(:name => "少女")
-# Category.create(:name => "大人")
-# Category.create(:name => "男性")
-# Category.create(:name => "女性")
-# Category.create(:name => "虚構")
-# Category.create(:name => "造物")
-# Category.create(:name => "動物")
-# Category.create(:name => "樹木")
-# Category.create(:name => "妹")
-
 require "csv"
 
-CSV.foreach('db/seeds/座の一覧(新).csv', headers: true) do |row|
-  Za.create(
+# CSV.foreach('db/seeds/座の一覧(新).csv', headers: true) do |row|
+#   Za.create(
+#     name: row['名称'],
+#     paranoia_limit: row['不安臨界'],
+#     image: File.open("./app/assets/images/#{row['画像名']}")
+#   )
+# end
+
+# CSV.foreach('db/seeds/分類の一覧.csv', headers: true) do |row|
+#   Category.create(
+#     name: row['名称'],
+#   )
+# end
+
+# CSV.foreach('db/seeds/座分類交差テーブルの一覧.csv', headers: true) do |row|
+#   ZaCategoryAttribution.create(
+#     za_id: row['座id'],
+#     category_id: row['分類id']
+#   )
+# end
+
+CSV.foreach('db/seeds/役職の一覧.csv', headers: true) do |row|
+  Position.create(
     name: row['名称'],
-    paranoia_limit: row['不安臨界'],
-    image: File.open("./app/assets/images/#{row['画像名']}")
+    max: row['最大'],
+    refusal: row['友好無視'].to_i,
+    unkillable: row['不死'].to_i,
+    obstinate: row['絶対犯'].to_i
   )
 end
-
-CSV.foreach('db/seeds/分類の一覧.csv', headers: true) do |row|
-  Category.create(
-    name: row['名称'],
-  )
-end
-
-CSV.foreach('db/seeds/座分類交差テーブルの一覧.csv', headers: true) do |row|
-  ZaCategoryAttribution.create(
-    za_id: row['座id'],
-    category_id: row['分類id']
-  )
-end
-
