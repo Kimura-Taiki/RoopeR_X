dfe2d3246114f4dd6b14a3efffcf0b7ed35e7c10
“Kimura-Taiki” <taiguimucun850@gmail.com>
2022-01-19 15:05:10 +0000

    脚本編集をindexと共通化、嵩張る点はモーダル化して圧縮してみようと思う

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -15,11 +15,15 @@ class ScriptsController < ApplicationController
   end
 
   def show
-    _id = params[:id].to_i
-    @script = Script.find(_id)
-    @pawns = Pawn.where(script_id: _id)
+    # _id = params[:id].to_i
+    # @script = Script.find(_id)
+    # @pawns = Pawn.where(script_id: _id)
+    @script = Script.find(params[:id])
+    @pawns = @script.pawns
     @pawn = Pawn.new
     @zas = Za.all
+    @weeks = [["１週", 1], ["２週", 2], ["３週", 3], ["４週", 4], ["５週", 5], ["６週", 6], ["７週", 7], ["８週", 8], ["９週", 9], ]
+    @days = [["１日", 1], ["２日", 2], ["３日", 3], ["４日", 4], ["５日", 5], ["６日", 6], ["７日", 7], ["８日", 8], ["９日", 9], ]
   end
 
   def edit
