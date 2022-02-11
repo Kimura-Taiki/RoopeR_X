154a5d6132414aa9179fd8be53bc72e275de70c5
“Kimura-Taiki” <taiguimucun850@gmail.com>
2022-02-11 14:37:27 +0000

    まずは座と役職のDBを更新

--- app/models/za.rb
+++ app/models/za.rb
@@ -6,4 +6,10 @@ class Za < ApplicationRecord
   has_many :pawns
 
   attachment :image
+
+  enum first_location: { waiting: 0, hospital: 1, shrine: 2, city: 3, school: 4, excluded: 5 }
+  # enum position_condition: { no_condition: 0, person_required: 1 }
+  # enum extra_argument: {
+  #   no_argument: 0, mystery_boy: 1, godly_being: 2, boss: 3, henchman: 4, scientist: 5, transfer_student: 6, copy_cat: 7, servant: 8
+  # }
 end
